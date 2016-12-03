924641f587145a05f4baa5e0c4481f64cd8ee4ba
ghbhaha <ghbhaha@gmail.com>
2016-12-03 20:29:22 +0800

    clear all button feature(1/2)
    Change-Id: Id51ee8c428fd3c8699ec82edc1efc28939e9ed71

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3771,6 +3771,19 @@ public final class Settings {
         public static final String SHOW_FOURG = "show_fourg";
 
         /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+
+        /**
          * I am the lolrus.
          * <p>
          * Nonzero values indicate that the user has a bukkit.
